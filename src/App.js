8c217ab95d0fc8370baf3b8d73e22ef5b0fb1489
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-04 03:52:07 +0600

    Registration Page initially added to App.js #16

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import {
 } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
 import Login from './views/users/Login'
+import Registration from './views/users/Registration'
 
 const App = () => {
     return(
         <Router>
             <Navbar/>
             <Route path="/login" component={Login}/>
+            <Route path="/register" component={Registration}/>
         </Router>
     )
 }
---
+++ src/views/users/Registration.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Registration = () => {
+    return(
+        "Registration"
+    )
+}
+
+export default Registration
