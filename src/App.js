a70b603558ab1373c9aa0b7e0bbb4402264ecc8f
rtlmhjbn <ratulmahjabin@iut-dhaka.edu>
2021-07-03 21:48:25 +0600

    update appJs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-
-import React from 'react'
+import React from "react";
 
 const App = () => {
-    return(
-        <div>
-            <h2>Hello from kishan ecomm</h2>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Footer</h2>
+    </div>
+  );
+};
 
-export default App
-
+export default App;
