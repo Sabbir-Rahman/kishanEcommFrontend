b299026b76aacc02646ace82b2b09fe3d71cb06a
rtlmhjbn <ratulmahjabin@iut-dhaka.edu>
2021-07-04 19:45:52 +0600

    update AppJs #10

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 import Login from "./views/users/Login";
 import Registration from "./views/users/Registration";
 
