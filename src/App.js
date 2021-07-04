03a49df259117a693279e3004a502fe5b4130faf
rtlmhjbn <ratulmahjabin@iut-dhaka.edu>
2021-07-04 19:39:09 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Login from "./views/users/Login";
 import Registration from "./views/users/Registration";
