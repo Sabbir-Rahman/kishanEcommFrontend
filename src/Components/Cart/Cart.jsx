011030f88b82a065357143ff043c2c16a599deb7
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-06 02:16:31 +0600

    E-comm page done initially #18

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-
 import CartItem from './CartItem/CartItem';
 import useStyles from './styles';
 
--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,3 @@
 import Commerce from '@chec/commerce.js';
 
-export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
+export const commerce = new Commerce("pk_test_19661763baf3194382812193e2e539617b1870f41d8b9", true);
