2f657839034d68aa83b561c129d4c57263e4d96c
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-06 23:28:11 +0600

    dashboard fix #38

--- src/Components/Navbar/NavbarDash/Navbar.js
+++ src/Components/Navbar/NavbarDash/Navbar.js
@@ -6,13 +6,13 @@ export default function Navbar(){
     return (
         <div class='ui top menu' className='navbar'>
             <div class='item'>
-                <h1>প্রোফাইল</h1><br/>
+                <h1>প্রোফাইল</h1>
+            </div>
+            <div class="item">
+                <a class='item'>ইউজারনেম</a>
             </div>
             <div class="right menu">
                 <Button color="green" fluid size="tiny">প্রোডাক্ট যোগ করুন</Button>
-            </div>
-            <div class="item">
-                <a class='item'><h1>My name is Shila</h1></a>
             </div>
         </div>
     )
