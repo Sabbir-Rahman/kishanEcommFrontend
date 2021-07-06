c36d6567adbc58ffb9dbf0e3fc5fc6dc9770bfd5
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-06 22:59:04 +0600

    dashboard sidebar navbar styling fix #38

--- src/Components/Navbar/NavbarDash/Navbar.js
+++ src/Components/Navbar/NavbarDash/Navbar.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import {Button} from 'semantic-ui-react'
+import "./style.css";
 
 export default function Navbar(){
     return (
-        <div class='ui top fixed menu'>
+        <div class='ui top menu' className='navbar'>
             <div class='item'>
                 <h1>প্রোফাইল</h1><br/>
             </div>
             <div class="right menu">
                 <Button color="green" fluid size="tiny">প্রোডাক্ট যোগ করুন</Button>
-                <a class='item right'>My name is Shila</a>
+            </div>
+            <div class="item">
+                <a class='item'><h1>My name is Shila</h1></a>
             </div>
         </div>
     )
