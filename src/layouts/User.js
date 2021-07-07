e7d6b77e8eb05b34e2f93dc92c8446b11cef69f2
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-08 01:49:24 +0600

    user layout #13

---
+++ src/layouts/User.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { useLocation, Route, Switch } from "react-router-dom";
+import AdminNavbar from "components/Navbars/AdminNavbar";
+import Footer from "components/Footer/Footer";
+import Sidebar from "components/Sidebar/Sidebar";
+import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
+import routes from "routes.js";
+import sidebarImage from "assets/img/sidebar-3.jpg";
+import MainNavbar from "components/Navbars/Navbar";
+
+function User() {
+  const [image, setImage] = React.useState(sidebarImage);
+  const [color, setColor] = React.useState("black");
+  const [hasImage, setHasImage] = React.useState(true);
+  const location = useLocation();
+  const mainPanel = React.useRef(null);
+  const getRoutes = (routes) => {
+    return routes.map((prop, key) => {
+      if (prop.layout === "/user") {
+        return (
+          <Route
+            path={prop.layout + prop.path}
+            render={(props) => <prop.component {...props} />}
+            key={key}
+          />
+        );
+      } else {
+        return null;
+      }
+    });
+  };
+  React.useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    mainPanel.current.scrollTop = 0;
+    if (
+      window.innerWidth < 993 &&
+      document.documentElement.className.indexOf("nav-open") !== -1
+    ) {
+      document.documentElement.classList.toggle("nav-open");
+      var element = document.getElementById("bodyClick");
+      element.parentNode.removeChild(element);
+    }
+  }, [location]);
+  return (
+    <>
+      <div className="wrapper">
+        <Sidebar color={color} image={hasImage ? image : ""} routes={routes} />
+        <div className="main-panel" ref={mainPanel}>
+          <MainNavbar/>
+          <AdminNavbar />
+          <div className="content">
+            <Switch>{getRoutes(routes)}</Switch>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default User;
