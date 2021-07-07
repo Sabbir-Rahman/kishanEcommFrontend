390051aceee85807092433bab8bfba1255217aad
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-08 01:34:24 +0600

    UserNavbar #6

---
+++ src/components/Navbars/UserNavbar.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import { useLocation } from "react-router-dom";
+import { Navbar, Container, Nav, Dropdown, Button } from "react-bootstrap";
+
+function Header() {
+  const location = useLocation();
+  const mobileSidebarToggle = (e) => {
+    e.preventDefault();
+    document.documentElement.classList.toggle("nav-open");
+    var node = document.createElement("div");
+    node.id = "bodyClick";
+    node.onclick = function () {
+      this.parentElement.removeChild(this);
+      document.documentElement.classList.toggle("nav-open");
+    };
+    document.body.appendChild(node);
+  };
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <div className="d-flex justify-content-center align-items-center ml-2 ml-lg-0">
+          <Button
+            variant="dark"
+            className="d-lg-none btn-fill d-flex justify-content-center align-items-center rounded-circle p-2"
+            onClick={mobileSidebarToggle}
+          >
+            <i className="fas fa-ellipsis-v"></i>
+          </Button>
+        </div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-2">
+          <span className="navbar-toggler-bar burger-lines"></span>
+          <span className="navbar-toggler-bar burger-lines"></span>
+          <span className="navbar-toggler-bar burger-lines"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="nav mr-auto" navbar>
+            <Nav.Item>
+              <Nav.Link
+                data-toggle="dropdown"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+                className="m-0"
+              >
+                <i className="nc-icon nc-palette"></i>
+                <span className="d-lg-none ml-1">Dashboard</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Dropdown as={Nav.Item}>
+              <Dropdown.Toggle
+                as={Nav.Link}
+                data-toggle="dropdown"
+                id="dropdown-67443507"
+                variant="default"
+                className="m-0"
+              >
+                <i className="nc-icon nc-planet"></i>
+                <span className="notification">5</span>
+                <span className="d-lg-none ml-1">Notification</span>
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  href="#pablo"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  Notification 1
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#pablo"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  Notification 2
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#pablo"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  Notification 3
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#pablo"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  Notification 4
+                </Dropdown.Item>
+                <Dropdown.Item
+                  href="#pablo"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  Another notification
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <Nav.Item>
+              <Nav.Link
+                className="m-0"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+              >
+                <i className="nc-icon nc-zoom-split"></i>
+                <span className="d-lg-block"> খোঁজ করুন</span>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Nav className="ml-auto" navbar>
+            <Nav.Item>
+              <Nav.Link
+                className="m-0"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+              >
+                <span className="no-icon">লগ আউট</span>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
