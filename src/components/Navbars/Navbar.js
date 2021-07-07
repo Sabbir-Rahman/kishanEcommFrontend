22fb320965d5b1406fa63e3f063d827a900cfe69
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-08 01:33:38 +0600

    Navbar #5

---
+++ src/components/Navbars/Navbar.js
@@ -0,0 +1,118 @@
+import React, { Component } from "react";
+import { useLocation } from "react-router-dom";
+import { Navbar, Container, Nav, Dropdown, Button } from "react-bootstrap";
+
+import routes from "routes.js";
+
+function MainNavbar() {
+  const location = useLocation();
+  const mobileSidebarToggle = (e) => {
+    e.preventDefault();
+    document.documentElement.classList.toggle("nav-open");
+    var node = document.createElement("div");
+    node.id = "bodyClick";
+    node.onclick = function () {
+      this.parentElement.removeChild(this);
+      document.documentElement.classList.toggle("nav-open");
+    };
+    document.body.appendChild(node);
+  };
+
+  const getBrandText = () => {
+    for (let i = 0; i < routes.length; i++) {
+      if (location.pathname.indexOf(routes[i].layout + routes[i].path) !== -1) {
+        return routes[i].name;
+      }
+    }
+    return "Brand";
+  };
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <div className="d-flex justify-content-center align-items-center ml-2 ml-lg-0">
+          <Button
+            variant="dark"
+            className="d-lg-none btn-fill d-flex justify-content-center align-items-center rounded-circle p-2"
+            onClick={mobileSidebarToggle}
+          >
+            <i className="fas fa-ellipsis-v"></i>
+          </Button>
+          <Navbar.Brand
+            href="#home"
+            onClick={(e) => e.preventDefault()}
+            className="mr-2"
+          >
+            {getBrandText()}
+          </Navbar.Brand>
+        </div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-2">
+          <span className="navbar-toggler-bar burger-lines"></span>
+          <span className="navbar-toggler-bar burger-lines"></span>
+          <span className="navbar-toggler-bar burger-lines"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="nav mr-auto" navbar>
+          <Nav.Item>
+              <Nav.Link
+                className="m-0"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+              >
+                <span className="no-icon">মূল পাতা</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                className="m-0"
+                href="/krishi-bazar"
+                onClick={(e) => e.preventDefault()}
+              >
+                <span className="no-icon">কৃষি বাজার</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                className="m-0"
+                href="/krishi-forum"
+                onClick={(e) => e.preventDefault()}
+              >
+                <span className="no-icon">কৃষি ফোরাম</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                className="m-0"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+              >
+                <span className="no-icon">ফসলের ডাক্তার</span>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Nav className="ml-auto" navbar>
+            <Nav.Item>
+              <Nav.Link
+                className="m-0"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+              >
+                <span className="no-icon">যোগ দিন</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                className="m-0"
+                href="#pablo"
+                onClick={(e) => e.preventDefault()}
+              >
+                <span className="no-icon">প্রবেশ করুন</span>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MainNavbar;
