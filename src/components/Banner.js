c9ab19964e2b8d9e92217e3ed814069fea0780e4
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-08 08:35:58 +0600

    Banner for krishibazar #24

---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div className="container">
+            <header>
+                <div className="headerText">
+                    <div>
+                        <h1>কৃষি বাঁচলে বাঁচবে দেশ</h1>
+                        <p>দেশের প্রথম ও একমাত্র কৃষি বিষয়ক ই-কমার্স ওয়েবসাইটে স্বাগতম</p>
+                    </div>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Banner
--- src/views/KrishiBazar.js
+++ src/views/KrishiBazar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import KrishiBazarNavbar from 'components/Navbars/KrishiBazarNavbar'
+import Banner from 'components/Banner'
 
 const KrishiBazar = () => {
     return(
         <div>
             <KrishiBazarNavbar/>
+            <Banner/>
         </div>
     )
 }
