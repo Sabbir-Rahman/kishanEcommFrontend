94ecc0e1bffd58a40fe6eb472d26de9ad4b03080
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-08 22:32:07 +0600

    Cart Fixed #26

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -23,7 +23,7 @@ const Cart = (props) => {
              
               dispatch({type: 'EMPTY'});
               props.history.push(`/`)
-              toast.success("You have paid successfully now you can continue your shopping!", {
+              toast.success("ট্রানজেকশন সফল হয়েছে!", {
                 position: toast.POSITION.TOP_RIGHT
               });
 
@@ -35,50 +35,61 @@ const Cart = (props) => {
    
     return(
        <div className="cartContainer">
-       <div className="cartDetails">
-           {shoppingCart.length ? shoppingCart.map(product => (
-       <div className="cart" key={product.id}>
-        <span className="cartProImage"><img src={product.image} alt=""/>
-           <span className="imageCount">{product.qty}</span>
-        </span>
-        <span className="cartProductName">{product.name}</span>
-        <span className="cartProductPrice">${product.price}.00</span>
-        <span className="inc" onClick={() => dispatch({type: 'INC', id:product.id})}><i className="fas fa-plus"></i></span>
-        <span className="productQuantity">{product.qty}</span>
-        <span className="dec" onClick={() => dispatch({type: 'DEC', id: product.id})}><i className="fas fa-minus"></i></span>
-        <span className="productTotalPrice">${product.qty * product.price}.00</span>
-        <button onClick={() => dispatch({type: 'DELETE_PRODUCT', id: product.id})} className="deleteCartPro"><i className="fas fa-trash-alt"></i></button>
-           </div>  
-        )) : 'Yourr Cart is currently empty!'}
-        
+           <div className="cartDetails">
+                {shoppingCart.length ? shoppingCart.map(product => (
+                    <div className="cart" key={product.id}>
+                        <span className="cartProImage">
+                            <img src={product.image} alt=""/>
+                           <span className="imageCount">
+                               {product.qty}
+                            </span>
+                        </span>
+                        <span className="cartProductName">
+                            {product.name}
+                        </span>
+                        <span className="cartProductPrice">
+                            {product.price}.00 টাকা
+                        </span>
+                        <span className="inc" onClick={() => dispatch({type: 'INC', id:product.id})}>
+                            <i className="fas fa-plus"></i>
+                        </span>
+                        <span className="productQuantity">
+                            {product.qty}
+                        </span>
+                        <span className="dec" onClick={() => dispatch({type: 'DEC', id: product.id})}>
+                            <i className="fas fa-minus"></i>
+                        </span>
+                        <span className="productTotalPrice">
+                            {product.qty * product.price}.00 টাকা
+                        </span>
+                        <button onClick={() => dispatch({type: 'DELETE_PRODUCT', id: product.id})} className="deleteCartPro">
+                            <i className="fas fa-trash-alt"></i>
+                        </button>
+                    </div>  
+                )) : 'Yourr Cart is currently empty!'}
+            </div>
+            {shoppingCart.length ? <div className="cartSummary">
+                <div className="summary">
+                    <h3>বিবরণ</h3>
+                    <div className="totalItems">
+                        <div className="items">সর্বমোট পণ্য</div>
+                        <div className="itemsCount">{qty}.00</div>
+                    </div>
+                    <div className="totalPriceSection">
+                        <div className="justTitle">সর্বমোট মূল্য</div>
+                            <div className="itemsPrice">{totalPrice}.00 টাকা</div>
+                    </div>
+                   <div className="stripSection">
+                    <StripeCheckout stripeKey="pk_test_HnF0cQhq9UGw2GvWRltNiAQM00kn9HlRCg" token={handleToken}
+                       billingAddress
+                       shippingAddress
+                       amount = {totalPrice * 100}
+                       name="all products in the cart"
+                    />
+                    </div>
+                </div>
+            </div> : ''}
         </div>
-        {shoppingCart.length ? <div className="cartSummary">
-            <div className="summary">
-                <h3>Order Summary</h3>
-                <div className="totalItems">
-                    <div className="items">Total Items</div>
-               <div className="itemsCount">{qty}.00</div>
-                </div>
-                <div className="totalPriceSection">
-                 <div className="justTitle">Total Price</div>
-        <div className="itemsPrice">${totalPrice}.00</div>
-                </div>
-       <div className="stripSection">
-       <StripeCheckout
-       stripeKey="pk_test_HnF0cQhq9UGw2GvWRltNiAQM00kn9HlRCg"
-       token={handleToken}
-       billingAddress
-       shippingAddress
-       amount = {totalPrice * 100}
-       name="all products in the cart"
-       />
-       </div>
-       </div></div>
-        : ''}
-      
-       
-       
-</div>
     )
 }
 
