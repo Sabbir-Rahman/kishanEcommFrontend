646aed41dc9eb90628f3f0985a1d47e54bc1610f
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-08 07:23:49 +0600

    Added views initially

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,8 @@
 import Dashboard from "views/Dashboard";
+import FosholerDoctor from "views/FosholerDoctor";
 import Home from "views/Home";
+import KrishiBazar from "views/KrishiBazar";
+import KrishiForum from "views/KrishiForum";
 import Login from "views/Login";
 import Register from "views/Registration";
 
@@ -15,21 +18,21 @@ const routes = [
     path: "/krishi-bazar",
     name: "কৃষি বাজার",
     icon: "nc-icon nc-sun-fog-29",
-    component: Register,
+    component: KrishiBazar,
     layout: ""
   },
   {
     path: "/krishi-forum",
     name: "কৃষি ফোরাম",
     icon: "nc-icon nc-paper-2",
-    component: Register,
+    component: KrishiForum,
     layout: ""
   },
   {
     path: "/fosholer-doctor",
     name: "ফসলের ডাক্তার",
     icon: "nc-icon nc-ambulance",
-    component: Register,
+    component: FosholerDoctor,
     layout: ""
   },
   {
---
+++ src/views/KrishiBazar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const KrishiBazar = () => {
+    return(
+        
+        <thead>
+        </thead>
+    )
+}
+
+export default KrishiBazar
