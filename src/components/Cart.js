fd688f56c45d05bb5c576fd4853d903447d50ad1
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-08 10:51:17 +0600

    #26 initial cart

---
+++ src/components/Cart.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Cart = () => {
+
+    return (
+        <div className="container">
+            <div className="cart-container">
+                <h2>Shopping Cart</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Cart
