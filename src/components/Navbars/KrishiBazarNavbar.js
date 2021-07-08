4e26665cb726f123dbd4d4d4a3c49a73fe8913ba
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-08 22:18:45 +0600

    Navbar fixed #23

--- src/components/Navbars/KrishiBazarNavbar.js
+++ src/components/Navbars/KrishiBazarNavbar.js
@@ -7,16 +7,30 @@ const KrishiBazarNavbar = ({cartToggle}) => {
     const {shoppingCart} = useContext(cartContext);
     return(
         <nav>
- <ul className="left">
- <li><Link to="/">Pak Express</Link></li>
- {/* <li><form>
-     <input type="text" className="search" placeholder="Search Product..."/>
-     </form></li> */}
- </ul>
- <ul className="right">
-    <li onClick={cartToggle}><Link to="/cart"><span className="dollor"><i className="fas fa-cart-plus"></i></span><span className="shoppingCartTotal">{shoppingCart ? shoppingCart.length : 0}</span></Link></li>
- </ul>
-</nav>
+            <ul className="left">
+                 <li>
+                     <Link to="/">কিষাণ</Link>
+                </li>
+                <li>
+                    <form>
+                        <input type="text" className="search" placeholder="পণ্য খুঁজুন..."/>
+                    </form>
+                </li>
+            </ul>
+            <ul className="right">
+                <li onClick={cartToggle}>
+                    <Link to="/cart">
+                        <span className="dollor">
+                            <i className="fas fa-cart-plus">
+                            </i>
+                        </span>
+                        <span className="shoppingCartTotal">
+                            {shoppingCart ? shoppingCart.length : 0}
+                        </span>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
