9db49ab7fd81209e8b92e193c93f22d4774e1e6d
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-08 07:35:47 +0600

    logo fixed

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -17,19 +17,14 @@ function Sidebar({ color, image, routes }) {
       />
       <div className="sidebar-wrapper">
         <div className="logo d-flex align-items-center justify-content-start">
-          <a
-            href="https://www.creative-tim.com?ref=lbd-sidebar"
-            className="simple-text logo-mini mx-1"
-          >
             <div className="logo-img">
               <img
-                src={require("assets/img/reactlogo.png").default}
+                src={require("assets/img/logo.png").default}
                 alt="..."
               />
             </div>
-          </a>
           <a className="simple-text" href="/">
-            কৃষাণ
+            <h1>কৃষাণ</h1>
           </a>
         </div>
         <Nav>
