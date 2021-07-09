f0cc10ff2a968a7cf42907f18587921010fddfc4
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-09 06:24:31 +0600

    finally sidebar color changed

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -9,16 +9,27 @@ function Sidebar({ color, image, routes }) {
   }
   return (
     <div className="sidebar" data-image={image} data-color={color}>
+      <div
+        className="sidebar-background"
+        style={{
+          backgroundImage: "url(" + image + ")",
+        }}
+      />
       <div className="sidebar-wrapper">
         <div className="logo d-flex align-items-center justify-content-start">
+          <a
+            href="https://www.creative-tim.com?ref=lbd-sidebar"
+            className="simple-text logo-mini mx-1"
+          >
             <div className="logo-img">
               <img
                 src={require("assets/img/logo.png").default}
                 alt="..."
               />
             </div>
+          </a>
           <a className="simple-text" href="/">
-            <h2>কৃষাণ</h2>
+            কৃষাণ
           </a>
         </div>
         <Nav>
