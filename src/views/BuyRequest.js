aa2514338f7f581468ac5a06b771eb87e22b35da
mohtasimhadi <mohtasimhadi@gmail.com>
2021-08-18 21:11:41 +0600

    buy req fixed

--- src/views/BuyRequest.js
+++ src/views/BuyRequest.js
@@ -12,7 +12,7 @@ import {
   Col,
 } from "react-bootstrap"
 
-function Maps() {
+function Messages() {
   return (
     <>
     <Container fluid>
@@ -26,135 +26,46 @@ function Maps() {
               <h5>
                 <small>ক্রয় সম্পন্নকৃত পণ্যসমূহ</small>
               </h5>
-              <Alert className="alert-with-icon" variant="info">
-                <button
-                  aria-hidden={true}
-                  className="close"
-                  data-dismiss="alert"
-                  type="button"
-                >
-                  <i className="nc-icon nc-simple-remove"></i>
-                </button>
-                <span
-                  data-notify="icon"
-                  className="nc-icon nc-bell-55"
-                ></span>
-                <span>
-                  পণ্য
-                </span>
-              </Alert>
-              <Alert className="alert-with-icon" variant="info">
-                <button
-                  aria-hidden={true}
-                  className="close"
-                  data-dismiss="alert"
-                  type="button"
-                >
-                  <i className="nc-icon nc-simple-remove"></i>
-                </button>
-                <span
-                  data-notify="icon"
-                  className="nc-icon nc-bell-55"
-                ></span>
-                <span>
-                  পণ্য
-                </span>
-              </Alert>
-              <Alert className="alert-with-icon" variant="info">
-                <button
-                  aria-hidden={true}
-                  className="close"
-                  data-dismiss="alert"
-                  type="button"
-                >
-                  <i className="nc-icon nc-simple-remove"></i>
-                </button>
-                <span
-                  data-notify="icon"
-                  className="nc-icon nc-bell-55"
-                ></span>
-                <span>
-                  পণ্য
-                </span>
-              </Alert>
-              <Alert className="alert-with-icon" variant="info">
-                <button
-                  aria-hidden={true}
-                  className="close"
-                  data-dismiss="alert"
-                  type="button"
-                >
-                  <i className="nc-icon nc-simple-remove"></i>
-                </button>
-                <span
-                  data-notify="icon"
-                  className="nc-icon nc-bell-55"
-                ></span>
-                <span>
-                  পণ্য
-                </span>
-              </Alert>
+              <Row style={{ background: "rgba(181, 218, 164, .3)", width: "100%", marginBottom: "5px", marginLeft: "5px", marginRight: "5px"}}>
+                <Col md="8" style={{color: "green", width: "80%"}}>পণ্যের নাম</Col>
+                <Col md="2" style={{marginRight: "0px"}}><Button >টিক</Button></Col>
+                <Col md="2" style={{marginRight: "0px"}}><Button>ক্রস</Button></Col>
+              </Row>
+
+              <Row style={{ background: "rgba(181, 218, 164, .3)", width: "100%", marginBottom: "5px", marginLeft: "5px", marginRight: "5px"}}>
+                <Col md="8" style={{color: "green", width: "80%"}}>পণ্যের নাম</Col>
+                <Col md="2" style={{marginRight: "0px"}}><Button >টিক</Button></Col>
+                <Col md="2" style={{marginRight: "0px"}}><Button>ক্রস</Button></Col>
+              </Row>
+
+              <Row style={{ background: "rgba(181, 218, 164, .3)", width: "100%", marginBottom: "5px", marginLeft: "5px", marginRight: "5px"}}>
+                <Col md="8" style={{color: "green", width: "80%"}}>পণ্যের নাম</Col>
+                <Col md="2" style={{marginRight: "0px"}}><Button >টিক</Button></Col>
+                <Col md="2" style={{marginRight: "0px"}}><Button>ক্রস</Button></Col>
+              </Row>
+
             </Col>
             <Col md="6">
               <h5>
                 <small>ক্রয় অসম্পন্ন পণ্যসমূহ</small>
               </h5>
-              <Alert variant="success">
-                <button
-                  aria-hidden={true}
-                  className="close"
-                  data-dismiss="alert"
-                  type="button"
-                >
-                  <i className="nc-icon nc-simple-remove"></i>
-                </button>
-                <span>
-                  <b>পণ্য -</b>
-                   পণ্য
-                </span>
-              </Alert>
-              <Alert variant="success">
-                <button
-                  aria-hidden={true}
-                  className="close"
-                  data-dismiss="alert"
-                  type="button"
-                >
-                  <i className="nc-icon nc-simple-remove"></i>
-                </button>
-                <span>
-                  <b>পণ্য -</b>
-                   পণ্য
-                </span>
-              </Alert>
-              <Alert variant="success">
-                <button
-                  aria-hidden={true}
-                  className="close"
-                  data-dismiss="alert"
-                  type="button"
-                >
-                  <i className="nc-icon nc-simple-remove"></i>
-                </button>
-                <span>
-                  <b>পণ্য -</b>
-                   পণ্য
-                </span>
-              </Alert>
-              <Alert variant="success">
-                <button
-                  aria-hidden={true}
-                  className="close"
-                  data-dismiss="alert"
-                  type="button"
-                >
-                  <i className="nc-icon nc-simple-remove"></i>
-                </button>
-                <span>
-                  <b>পণ্য -</b>
-                   পণ্য
-                </span>
-              </Alert>
+              <Row style={{ background: "rgba(181, 218, 164, .3)", width: "100%", marginBottom: "5px", marginLeft: "5px", marginRight: "5px"}}>
+                <Col md="8" style={{color: "green", width: "80%"}}>পণ্যের নাম</Col>
+                <Col md="2" style={{marginRight: "0px"}}><Button >টিক</Button></Col>
+                <Col md="2" style={{marginRight: "0px"}}><Button>ক্রস</Button></Col>
+              </Row>
+
+              <Row style={{ background: "rgba(181, 218, 164, .3)", width: "100%", marginBottom: "5px", marginLeft: "5px", marginRight: "5px"}}>
+                <Col md="8" style={{color: "green", width: "80%"}}>পণ্যের নাম</Col>
+                <Col md="2" style={{marginRight: "0px"}}><Button >টিক</Button></Col>
+                <Col md="2" style={{marginRight: "0px"}}><Button>ক্রস</Button></Col>
+              </Row>
+
+              <Row style={{ background: "rgba(181, 218, 164, .3)", width: "100%", marginBottom: "5px", marginLeft: "5px", marginRight: "5px"}}>
+                <Col md="8" style={{color: "green", width: "80%"}}>পণ্যের নাম</Col>
+                <Col md="2" style={{marginRight: "0px"}}><Button >টিক</Button></Col>
+                <Col md="2" style={{marginRight: "0px"}}><Button>ক্রস</Button></Col>
+              </Row>
             </Col>
             </Row>
         </Card.Body>
@@ -164,4 +75,4 @@ function Maps() {
   )
 }
 
-export default Maps;
+export default Messages;
