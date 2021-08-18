62713a1b98aebbba32e058ab30b854db2734e2b3
Ratul Mahjabin <ratulmahjabin@iut-dhaka.edu>
2021-08-18 12:56:09 +0600

    Update SellRequest.js

--- src/views/SellRequest.js
+++ src/views/SellRequest.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 import {
   Alert,
   Badge,
@@ -10,153 +10,180 @@ import {
   Container,
   Row,
   Col,
-} from "react-bootstrap"
+} from 'react-bootstrap'
+import axios from 'axios'
+import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+// data fetch
 
 function Sellrequset() {
+  const [requests, setRequest] = useState([])
+
+  const ShowRequest = async () => {
+    const token = localStorage.getItem('user')
+
+    const _token = token.split('"').join('')
+    console.log(token)
+    //console.log(`Bearer ` + _token)
+    const config = {
+      headers: {
+        Authorization: `Bearer ${_token}`,
+      },
+    }
+
+    axios
+      .get('http://127.0.0.1:5000/product/order/buyRequest', config)
+      .then((res) => {
+        console.log('RESPONSE RECEIVED: ', res)
+        setRequest(res.data)
+        alert('Data fetched')
+      })
+      .catch((err) => {
+        console.log('AXIOS ERROR: ', err)
+      })
+  }
+
+  useEffect(async () => {
+    ShowRequest()
+  }, [])
+
+  // data fetched.. request.data
   return (
     <>
       <Container fluid>
         <Card>
           <Card.Header>
-            <Card.Title as="h4">পণ্য বিক্রয় অনুরোধ</Card.Title>
+            <Card.Title as='h4'>পণ্য বিক্রয় অনুরোধ</Card.Title>
           </Card.Header>
           <Card.Body>
             <Row>
-              <Col md="6">
+              <Col md='6'>
                 <h5>
                   <small>বিক্রয় সম্পন্নকৃত পণ্যসমূহ</small>
                 </h5>
-                <Alert className="alert-with-icon" variant="info">
+                <Alert className='alert-with-icon' variant='info'>
                   <button
                     aria-hidden={true}
-                    className="close"
-                    data-dismiss="alert"
-                    type="button"
+                    className='close'
+                    data-dismiss='alert'
+                    type='button'
                   >
-                    <i className="nc-icon nc-simple-remove"></i>
+                    <i className='nc-icon nc-simple-remove'></i>
                   </button>
                   <span
-                    data-notify="icon"
-                    className="nc-icon nc-bell-55"
+                    data-notify='icon'
+                    className='nc-icon nc-bell-55'
                   ></span>
-                  <span>
-                    পণ্য
-                  </span>
+                  <span>পণ্য</span>
                 </Alert>
-                <Alert className="alert-with-icon" variant="info">
+                <Alert className='alert-with-icon' variant='info'>
                   <button
                     aria-hidden={true}
-                    className="close"
-                    data-dismiss="alert"
-                    type="button"
+                    className='close'
+                    data-dismiss='alert'
+                    type='button'
                   >
-                    <i className="nc-icon nc-simple-remove"></i>
+                    <i className='nc-icon nc-simple-remove'></i>
                   </button>
                   <span
-                    data-notify="icon"
-                    className="nc-icon nc-bell-55"
+                    data-notify='icon'
+                    className='nc-icon nc-bell-55'
                   ></span>
-                  <span>
-                    পণ্য
-                  </span>
+                  <span>পণ্য</span>
                 </Alert>
-                <Alert className="alert-with-icon" variant="info">
+                <Alert className='alert-with-icon' variant='info'>
                   <button
                     aria-hidden={true}
-                    className="close"
-                    data-dismiss="alert"
-                    type="button"
+                    className='close'
+                    data-dismiss='alert'
+                    type='button'
                   >
-                    <i className="nc-icon nc-simple-remove"></i>
+                    <i className='nc-icon nc-simple-remove'></i>
                   </button>
                   <span
-                    data-notify="icon"
-                    className="nc-icon nc-bell-55"
+                    data-notify='icon'
+                    className='nc-icon nc-bell-55'
                   ></span>
-                  <span>
-                    পণ্য
-                  </span>
+                  <span>পণ্য</span>
                 </Alert>
-                <Alert className="alert-with-icon" variant="info">
+                <Alert className='alert-with-icon' variant='info'>
                   <button
                     aria-hidden={true}
-                    className="close"
-                    data-dismiss="alert"
-                    type="button"
+                    className='close'
+                    data-dismiss='alert'
+                    type='button'
                   >
-                    <i className="nc-icon nc-simple-remove"></i>
+                    <i className='nc-icon nc-simple-remove'></i>
                   </button>
                   <span
-                    data-notify="icon"
-                    className="nc-icon nc-bell-55"
+                    data-notify='icon'
+                    className='nc-icon nc-bell-55'
                   ></span>
-                  <span>
-                    পণ্য
-                  </span>
+                  <span>পণ্য</span>
                 </Alert>
               </Col>
-              <Col md="6">
+              <Col md='6'>
                 <h5>
                   <small>বিক্রয় অসম্পন্ন পণ্যসমূহ</small>
                 </h5>
-                <Alert variant="success">
+                <Alert variant='success'>
                   <button
                     aria-hidden={true}
-                    className="close"
-                    data-dismiss="alert"
-                    type="button"
+                    className='close'
+                    data-dismiss='alert'
+                    type='button'
                   >
-                    <i className="nc-icon nc-simple-remove"></i>
+                    <i className='nc-icon nc-simple-remove'></i>
                   </button>
                   <span>
                     <b>পণ্য -</b>
-                     পণ্য
+                    পণ্য
                   </span>
                 </Alert>
-                <Alert variant="success">
+                <Alert variant='success'>
                   <button
                     aria-hidden={true}
-                    className="close"
-                    data-dismiss="alert"
-                    type="button"
+                    className='close'
+                    data-dismiss='alert'
+                    type='button'
                   >
-                    <i className="nc-icon nc-simple-remove"></i>
+                    <i className='nc-icon nc-simple-remove'></i>
                   </button>
                   <span>
                     <b>পণ্য -</b>
-                     পণ্য
+                    পণ্য
                   </span>
                 </Alert>
-                <Alert variant="success">
+                <Alert variant='success'>
                   <button
                     aria-hidden={true}
-                    className="close"
-                    data-dismiss="alert"
-                    type="button"
+                    className='close'
+                    data-dismiss='alert'
+                    type='button'
                   >
-                    <i className="nc-icon nc-simple-remove"></i>
+                    <i className='nc-icon nc-simple-remove'></i>
                   </button>
                   <span>
                     <b>পণ্য -</b>
-                     পণ্য
+                    পণ্য
                   </span>
                 </Alert>
-                <Alert variant="success">
+                <Alert variant='success'>
                   <button
                     aria-hidden={true}
-                    className="close"
-                    data-dismiss="alert"
-                    type="button"
+                    className='close'
+                    data-dismiss='alert'
+                    type='button'
                   >
-                    <i className="nc-icon nc-simple-remove"></i>
+                    <i className='nc-icon nc-simple-remove'></i>
                   </button>
                   <span>
                     <b>পণ্য -</b>
-                     পণ্য
+                    পণ্য
                   </span>
                 </Alert>
               </Col>
-              </Row>
+            </Row>
           </Card.Body>
         </Card>
       </Container>
@@ -164,4 +191,4 @@ function Sellrequset() {
   )
 }
 
-export default Sellrequset;
+export default Sellrequset
