793d3c6b4aaa159633d5ce11d125dfbf3499d7a1
Ratul Mahjabin <ratulmahjabin@iut-dhaka.edu>
2021-08-19 22:25:48 +0600

    updated

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react"
-import { useLocation, Route, Switch, Redirect } from "react-router-dom"
-import Sidebar from "components/Sidebar/Sidebar"
-import routes from "routes/routes.js"
-import sidebarImage from "assets/img/sidebar-3.jpg"
-import Header from "components/Navbars/Navbar"
-import AuthenticatedHeader from "components/Navbars/Navbar"
+import React, { Component } from 'react'
+import { useLocation, Route, Switch, Redirect } from 'react-router-dom'
+import Sidebar from 'components/Sidebar/Sidebar'
+import routes from 'routes/routes.js'
+import sidebarImage from 'assets/img/sidebar-3.jpg'
+import Header from 'components/Navbars/Navbar'
+import AuthenticatedHeader from 'components/Navbars/Navbar'
 
 function Home() {
   const [image, setImage] = React.useState(sidebarImage)
-  const [color, setColor] = React.useState("black")
+  const [color, setColor] = React.useState('black')
   const [hasImage, setHasImage] = React.useState(true)
   const location = useLocation()
   const mainPanel = React.useRef(null)
   const getRoutes = (routes) => {
     return routes.map((prop, key) => {
-      if (prop.layout === "") {
+      if (prop.layout === '') {
         return (
           <Route
             path={prop.layout + prop.path}
@@ -23,29 +23,32 @@ function Home() {
           />
         )
       } else {
-        return null;
+        return null
       }
     })
   }
 
-  if("authenticated"){
+  const token = localStorage.getItem('user')
+  console.log(token)
+
+  if (token) {
     return (
-      <div style={{ backgroundColor: "#F4FFEE" }}>
-        <AuthenticatedHeader/>
-          <Switch>
-            {getRoutes(routes)}
-            <Redirect from="/" to="/home" />
-          </Switch>
+      <div style={{ backgroundColor: '#F4FFEE' }}>
+        <AuthenticatedHeader />
+        <Switch>
+          {getRoutes(routes)}
+          <Redirect from='/' to='/home' />
+        </Switch>
       </div>
     )
   } else {
     return (
-      <div style={{ backgroundColor: "#F4FFEE" }}>
-        <Header/>
-          <Switch>
-            {getRoutes(routes)}
-            <Redirect from="/" to="/home" />
-          </Switch>
+      <div style={{ backgroundColor: '#F4FFEE' }}>
+        <Header />
+        <Switch>
+          {getRoutes(routes)}
+          <Redirect from='/' to='/home' />
+        </Switch>
       </div>
     )
   }
