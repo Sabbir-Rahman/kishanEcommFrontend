33bb511182889c8693dcb430470ff5f31f0e3066
Ratul Mahjabin <ratulmahjabin@iut-dhaka.edu>
2021-10-05 23:56:19 +0600

    update

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,31 +7,25 @@ import {
   Row,
   Col,
   Form,
-  ButtonGroup
+  ButtonGroup,
 } from 'react-bootstrap'
 import Dropdown from 'react-bootstrap/Dropdown'
 import bgImage from 'assets/img/bgKrishiBazar.jpg'
-import { Link, useHistory } from "react-router-dom"
+import { Link, useHistory } from 'react-router-dom'
 import { useState } from 'react'
 import { Modal } from 'react-bootstrap'
 import Product from './Product'
 
-
 const Cart = () => {
-  
-  function Redirection () {
+  function Redirection() {
     // localStorage.setItem('productID', id)
-    console.log("Hi")
-    return (
-      <></>
-    )
+    console.log('Hi')
+    return <></>
   }
 
-  function anotherOne(){
+  function anotherOne() {
     console.log('Hello')
   }
-  
-  
 
   let id = []
   let seller_id = []
@@ -60,33 +54,35 @@ const Cart = () => {
 
   const { products } = useContext(productContext)
 
-  products.map((product) => (
-    id.push(product.id),
-    name.push(product.name),
-    seller_id.push(product.seller_id),
-    description.push(product.description),
-    category.push(product.catagory),
-    subCategory.push(product.subCategory),
-    image.push(product.image),
-    image2.push(product.image2),
-    image3.push(product.image3),
-    unitName.push(product.unitName),
-    unitPrize.push(product.unitPrize),
-    bookingPercentage.push(product.bookingPercentage),
-    available.push(product.available),
-    minOrder.push(product.minOrder),
-    rating.push(product.rating),
-    availableDate.push(product.availableDate),
-    division.push(product.division),
-    district.push(product.district),
-    upazilla.push(product.upazilla),
-    isVerified.push(product.isVerified),
-    isAvailableNow.push(product.isAvailableNow),
-    rating.push(product.rating),
-    comments.push(product.comments),
-    question.push(product.question),
-    timestamp.push(product.timestamp)
-  ))
+  products.map(
+    (product) => (
+      id.push(product.id),
+      name.push(product.name),
+      seller_id.push(product.seller_id),
+      description.push(product.description),
+      category.push(product.catagory),
+      subCategory.push(product.subCategory),
+      image.push(product.image),
+      image2.push(product.image2),
+      image3.push(product.image3),
+      unitName.push(product.unitName),
+      unitPrize.push(product.unitPrize),
+      bookingPercentage.push(product.bookingPercentage),
+      available.push(product.available),
+      minOrder.push(product.minOrder),
+      rating.push(product.rating),
+      availableDate.push(product.availableDate),
+      division.push(product.division),
+      district.push(product.district),
+      upazilla.push(product.upazilla),
+      isVerified.push(product.isVerified),
+      isAvailableNow.push(product.isAvailableNow),
+      rating.push(product.rating),
+      comments.push(product.comments),
+      question.push(product.question),
+      timestamp.push(product.timestamp)
+    )
+  )
 
   var cart = []
   for (let i = 0; i < id.length; i++) {
@@ -112,7 +108,9 @@ const Cart = () => {
           />
         </Card.Header>
         <Card.Body>
-          <p style={{ marginTop: '10px', fontSize: '20px', marginBottom: "0px" }}>
+          <p
+            style={{ marginTop: '10px', fontSize: '20px', marginBottom: '0px' }}
+          >
             {name[i]}
           </p>
           <p
@@ -120,34 +118,27 @@ const Cart = () => {
               marginTop: '0px',
               color: 'grey',
               fontSize: '12px',
-              marginBottom: "0px"
+              marginBottom: '0px',
             }}
           >
             সর্বনিম্ন অর্ডার {minOrder[i]} {unitName[i]}
           </p>
-          <p style={{ fontSize: '15px', color: 'green', marginBottom: "0px" }}>
+          <p style={{ fontSize: '15px', color: 'green', marginBottom: '0px' }}>
             মূল্য: {unitPrize[i]} টাকা/{unitName[i]}
           </p>
         </Card.Body>
-        <Link to="/product">
-            <Row>
-              <div className='proButton'>
-                <button onClick={Redirection()}>
-                  পণ্য দেখুন
-                  {anotherOne()}
-                </button>
-              </div>
-            </Row>
+        <Link to='/product'>
+          <Row>
+            <div className='proButton'>
+              <button onClick={Redirection}>পণ্য দেখুন</button>
+            </div>
+          </Row>
         </Link>
       </Card>
     )
   }
 
-  return (
-    <>
-      {cart}
-    </>
-  )
+  return <>{cart}</>
 }
 
 export default Cart
