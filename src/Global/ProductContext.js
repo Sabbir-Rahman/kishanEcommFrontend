a5ffd6b6728727baa150e4af9573dd3bfe8cebc4
mohtasimhadi <mohtasimhadi@gmail.com>
2021-10-06 06:18:49 +0600

    Refactored Product Context

--- src/Global/ProductContext.js
+++ src/Global/ProductContext.js
@@ -25,77 +25,38 @@ const ProductContextProvider = (props) => {
     val = productss.data
   }
 
-  let id = []
-  let seller_id = []
-  let name = []
-  let description = []
-  let category = []
-  let subCategory = []
-  let image = []
-  let image2 = []
-  let image3 = []
-  let unitName = []
-  let unitPrize = []
-  let bookingPercentage = []
-  let available = []
-  let minOrder = []
-  let availableDate = []
-  let division = []
-  let district = []
-  let upazilla = []
-  let isVerified = []
-  let isAvailableNow = []
-  let rating = []
-  let comments = []
-  let question = []
-  let timestamp = []
 
-  for (let i = 0; i < val.length; i++) {
-    id.push(val[i]._id)
-    name.push(val[i].name)
-    seller_id.push(val[i].seller_id)
-    description.push(val[i].description)
-    category.push(val[i].catagory)
-    subCategory.push(val[i].subCategory)
-    image.push(val[i].image)
-    image2.push(val[i].image2)
-    image3.push(val[i].image3)
-    unitName.push(val[i].unitName)
-    unitPrize.push(val[i].unitPrize)
-    bookingPercentage.push(val[i].bookingPercentage)
-    available.push(val[i].available)
-    minOrder.push(val[i].minOrder)
-    rating.push(val[i].rating)
-    availableDate.push(val[i].availableDate)
-    division.push(val[i].division)
-    district.push(val[i].district)
-    upazilla.push(val[i].upazilla)
-    isVerified.push(val[i].isVerified)
-    isAvailableNow.push(val[i].isAvailableNow)
-    rating.push(val[i].rating)
-    comments.push(val[i].comments)
-    question.push(val[i].question)
-    timestamp.push(val[i].timestamp)
-  }
-
+  
   var products = []
-  for (let i = 0; i < val.length; i++) {
+  val.map((v)=>(
     products.push({
-      id: id[i],
-      name: name[i],
-      price: unitPrize[i],
-      unitName: unitName[i],
-      image: image[i],
-      category: category[i],
-      minOrder: minOrder[i],
-      bookingPercentage: bookingPercentage[i],
-      division: division[i],
-      district: district[i],
-      availableDate: availableDate[i],
-      upazilla: upazilla[i],
-      available: available[i],
+    id: v._id,
+    name: v.name,
+    seller_id: v.seller_i,
+    description: v.description,
+    category: v.catagory,
+    subCategory: v.subCategory,
+    image: v.image,
+    image2: v.image2,
+    image3: v.image3,
+    unitName: v.unitName,
+    unitPrize: v.unitPrize,
+    bookingPercentage: v.bookingPercentage,
+    available: v.available,
+    minOrder: v.minOrder,
+    rating: v.rating,
+    availableDate: v.availableDate,
+    division: v.division,
+    district: v.district,
+    upazilla: v.upazilla,
+    isVerified: v.isVerified,
+    isAvailableNow: v.isAvailableNow,
+    rating: v.rating,
+    comments: v.comments,
+    question: v.question,
+    timestamp: v.timestamp
     })
-  }
+  ))
 
   return (
     <productContext.Provider value={{ products: [...products] }}>
