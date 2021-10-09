3a6790a4c86159789be39ca52f4e7ba63c5a8c34
mohtasimhadi <mohtasimhadi@gmail.com>
2021-10-09 21:57:34 +0600

    Refactored navbar code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,33 +2,6 @@ import React from 'react'
 import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap'
 
 function Header() {
-    const mobileSidebarToggle = (e) => {
-        e.preventDefault()
-        document.documentElement.classList.toggle("nav-open");
-        var node = document.createElement("div")
-        node.id = "bodyClick"
-        node.onclick = function () {
-            this.parentElement.removeChild(this)
-            document.documentElement.classList.toggle("nav-open");
-        }
-        document.body.appendChild(node);
-    }
-
-    function logout() {
-        localStorage.clear()
-    }
-
-    function navLinks(href, name) {
-
-        return (
-            <Nav.Item>
-                <Nav.Link href={href}>
-                    <span className="no-icon">{name}</span>
-                </Nav.Link>
-            </Nav.Item>
-        )
-
-    }
 
     const token = localStorage.getItem('user')
     const role = localStorage.getItem('user_role')
@@ -58,24 +31,25 @@ function Header() {
             <NavDropdown.Item href="/register">রেজিস্ট্রেশন</NavDropdown.Item>
         </NavDropdown>
     }
+    
+    function logout() {
+        localStorage.clear()
+    }
+
+    function navLinks(href, name) {
+        return (
+            <Nav.Item>
+                <Nav.Link href={href}>
+                    <span className="no-icon">{name}</span>
+                </Nav.Link>
+            </Nav.Item>
+        )
+
+    }
 
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Container fluid>
-                <div className="d-flex justify-content-center align-items-center ml-2 ml-lg-0">
-                    <Button
-                        variant="dark"
-                        className="d-lg-none btn-fill d-flex justify-content-center align-items-center rounded-circle p-2"
-                        onClick={mobileSidebarToggle}
-                    >
-                        <i className="fas fa-ellipsis-v"></i>
-                    </Button>
-                </div>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-2">
-                    <span className="navbar-toggler-bar burger-lines"></span>
-                    <span className="navbar-toggler-bar burger-lines"></span>
-                    <span className="navbar-toggler-bar burger-lines"></span>
-                </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="nav mr-auto" navbar>
                         {navLinks("/", "মূলপাতা")}
