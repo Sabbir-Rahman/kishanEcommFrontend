e03ee3b09744a2f0fedd6ca1d1e8dd10ed418e29
Ratul Mahjabin <ratulmahjabin@iut-dhaka.edu>
2021-10-10 21:59:43 +0600

    'update'

--- src/views/KrishiBazar.js
+++ src/views/KrishiBazar.js
@@ -1,79 +1,81 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import {
-  Button,
-  Card,
-  Row,
-  Col,
-  Form,
-} from 'react-bootstrap'
+import { Button, Card, Row, Col, Form } from 'react-bootstrap'
 import bgImage from 'assets/img/bgKrishiBazar.jpg'
 import Cart from 'components/Cart'
-
+import { useState } from 'react'
 
 const KrishiBazar = () => {
+  const [link, setLink] = useState(null)
 
-  const cart = Cart(null)
+  let cart = Cart(link)
+
+  function printt() {
+    //cart = Cart(null)
+    console.log(link)
+  }
 
   return (
-      <Router>
-        <Switch>
-          <Route path='/krishi-bazar' exact>
-            <>
-              <Card style={{
+    <Router>
+      <Switch>
+        <Route path='/krishi-bazar' exact>
+          <>
+            <Card
+              style={{
                 marginTop: '2px',
                 marginLeft: '10px',
-                marginRight: '10px'
-              }}>
-                <Row
-                  style={{
-                    marginTop: '25px',
-                    height: '250px',
-                    marginBottom: '25px',
-                    marginLeft: '5px',
-                    marginRight: '5px',
-                  }}
-                >
-
-                  <Col>
-                    <Card
+                marginRight: '10px',
+              }}
+            >
+              <Row
+                style={{
+                  marginTop: '25px',
+                  height: '250px',
+                  marginBottom: '25px',
+                  marginLeft: '5px',
+                  marginRight: '5px',
+                }}
+              >
+                <Col>
+                  <Card
+                    style={{
+                      width: '100%',
+                      height: '60%',
+                      backgroundColor: 'rgba(181, 218, 164, .3)',
+                      backgroundSize: 'cover',
+                      display: 'flex',
+                      color: '#000',
+                      position: 'relative',
+                      textAlign: 'absolute',
+                    }}
+                  >
+                    <img
+                      src={bgImage}
+                      alt='Not found'
                       style={{
+                        background: 'white',
                         width: '100%',
-                        height: '60%',
-                        backgroundColor: 'rgba(181, 218, 164, .3)',
-                        backgroundSize: 'cover',
-                        display: 'flex',
-                        color: '#000',
-                        position: 'relative',
-                        textAlign: 'absolute',
+                        height: '100%',
+                        overflow: 'hidden',
                       }}
+                    />
+                  </Card>
+                </Col>
+              </Row>
+              <Row md='auto' style={{ marginLeft: '2%', marginRight: '2%' }}>
+                <Col>
+                  <Card className='card-stats'>
+                    <Card.Title
+                      as='h4'
+                      style={{ marginTop: 10, marginLeft: 15 }}
                     >
-                      <img
-                        src={bgImage}
-                        alt='Not found'
-                        style={{
-                          background: 'white',
-                          width: '100%',
-                          height: '100%',
-                          overflow: 'hidden',
-                        }}
-                      />
-                    </Card>
-                  </Col>
-                </Row>
-                <Row md='auto' style={{ marginLeft: '2%', marginRight: '2%' }}>
-                  <Col>
-                    <Card className='card-stats' >
-                      <Card.Title as='h4' style={{ marginTop: 10, marginLeft: 15 }}>
-                        বিভাগ
-                      </Card.Title>
-                      <Card.Body>
-                        <Form>
-                          <Form.Group controlId='division'>
-                            <Form.Label>বিভাগ</Form.Label>
-                            <select class="form-control"
-                            as='select'
-                          >
+                      বিভাগ
+                    </Card.Title>
+                    <Card.Body>
+                      <Form>
+                        <Form.Group controlId='division'>
+                          <Form.Label>বিভাগ</Form.Label>
+                          <select class='form-control' as='select'>
                             <option value=''></option>
                             <option value='Dhaka'>ঢাকা</option>
                             <option value='Mymensingh'>ময়মনসিংহ</option>
@@ -84,82 +86,117 @@ const KrishiBazar = () => {
                             <option value='Rajshahi'>রাজশাহী</option>
                             <option value='Chattagram'>চট্টগ্রাম</option>
                           </select>
-                          </Form.Group>
-                          <Button
-                            variant='success'
-                            style={{ marginBottom: 10, marginLeft: 110 }}
-                          >
-                            সার্চ করুন
-                          </Button>
-                        </Form>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                  <Col>
-                    <Card className='card-stats' >
-                      <Card.Title as='h4' style={{ marginTop: 10, marginLeft: 15 }}>
-                        ক্যাটাগরি
-                      </Card.Title>
-                      <Card.Body>
-                        <Form>
-                          <Form.Group controlId='division'>
-                            <Form.Label>ক্যাটাগরি</Form.Label>
-                            <select class="form-control"
+                        </Form.Group>
+                        <Button
+                          variant='success'
+                          style={{ marginBottom: 10, marginLeft: 110 }}
+                        >
+                          সার্চ করুন
+                        </Button>
+                      </Form>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col>
+                  <Card className='card-stats'>
+                    <Card.Title
+                      as='h4'
+                      style={{ marginTop: 10, marginLeft: 15 }}
+                    >
+                      ক্যাটাগরি
+                    </Card.Title>
+                    <Card.Body>
+                      <Form>
+                        <Form.Group controlId='division'>
+                          <Form.Label>ক্যাটাগরি</Form.Label>
+                          <select
+                            class='form-control'
                             as='select'
+                            value={link}
+                            onChange={(e) => setLink(e.target.value)}
                           >
                             <option value=''></option>
-                            <option value='fruit'>ফল</option>
-                            <option value='vegetable'>সবজি</option>
-                            <option value='oil'>তেল</option>
-                            <option value='meat'>মাংস</option>
-                            <option value='seeds'>বীজ</option>
-                            <option value='dairy'>দুগ্ধজাত পণ্য</option>
-                            <option value='egg'>ডিম</option>
-                            <option value='rice'>চাল</option>
-                            <option value='wheat'>গম</option>
-                            <option value='cotton'>তুলা</option>
-                            <option value='spices'>মশলা</option>
-                            <option value='poultry'>পোল্ট্রি</option>
-                            <option value='teaNcoffe'>চা ও কফি</option>
-                            <option value='Others'>অন্যান্য</option>
+                            <option value='http://127.0.0.1:5000/product/view?category=fruit'>
+                              ফল
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=vegetable'>
+                              সবজি
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=oil'>
+                              তেল
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=meat'>
+                              মাংস
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=seeds'>
+                              বীজ
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=dairy'>
+                              দুগ্ধজাত পণ্য
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=egg'>
+                              ডিম
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=rice'>
+                              চাল
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=wheat'>
+                              গম
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=cotton'>
+                              তুলা
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=spices'>
+                              মশলা
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=poultry'>
+                              পোল্ট্রি
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=teaNcoffe'>
+                              চা ও কফি
+                            </option>
+                            <option value='http://127.0.0.1:5000/product/view?category=Others'>
+                              অন্যান্য
+                            </option>
                           </select>
-                          </Form.Group>
-                          <Button
-                            variant='success'
-                            style={{ marginBottom: 10, marginLeft: 110 }}
-                          >
-                            সার্চ করুন
-                          </Button>
-                        </Form>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                </Row>
-              </Card>
-              <Card style={{ marginLeft: 10, marginRight: 10 }}>
-                <Row>
-                  <Col style={{ marginRight: 0, marginLeft: 0 }}>
-                    <Card md='auto'>
-                      <Card.Body
-                        style={{
-                          marginTop: '1%',
-                          marginLeft: '1%',
-                          marginRight: '1%',
-                          display: 'flex',
-                          flexWrap: 'wrap',
-                          overflow: 'hidden',
-                        }}
-                      >
-                        {cart}
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                </Row>
-              </Card>
-            </>
-          </Route>
-        </Switch>
-      </Router>
+                        </Form.Group>
+                        <Button
+                          variant='success'
+                          style={{ marginBottom: 10, marginLeft: 110 }}
+                          onClick={printt}
+                        >
+                          সার্চ করুন
+                        </Button>
+                      </Form>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Card>
+            <Card style={{ marginLeft: 10, marginRight: 10 }}>
+              <Row>
+                <Col style={{ marginRight: 0, marginLeft: 0 }}>
+                  <Card md='auto'>
+                    <Card.Body
+                      style={{
+                        marginTop: '1%',
+                        marginLeft: '1%',
+                        marginRight: '1%',
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        overflow: 'hidden',
+                      }}
+                    >
+                      {cart}
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Card>
+          </>
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
