9525cb663c6fb07c97ad3b8c817c206e6917ccf9
Ratul Mahjabin <ratulmahjabin@iut-dhaka.edu>
2021-10-15 01:16:57 +0600

    'update'

--- src/Global/ProductData.js
+++ src/Global/ProductData.js
@@ -2,65 +2,71 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function productData(param) {
+  const [productss, setProduct] = useState([])
 
-    const [productss, setProduct] = useState([])
-
-    const ShowProduct = param==null? async () => {
-        const token = localStorage.getItem('user')
-        await axios
+  const ShowProduct =
+    param == null
+      ? async () => {
+          const token = localStorage.getItem('user')
+          await axios
             .get('http://127.0.0.1:5000/product/view', {
-                headers: { Authorization: token },
+              headers: { Authorization: token },
             })
             .then((res) => setProduct(res.data))
-    } : async () => {
-        const token = localStorage.getItem('user')
-        await axios
+        }
+      : async () => {
+          const token = localStorage.getItem('user')
+          await axios
             .get(param, {
-                headers: { Authorization: token },
+              headers: { Authorization: token },
             })
             .then((res) => setProduct(res.data))
+        }
+
+  useEffect(async () => {
+    ShowProduct()
+  }, [])
+
+  let val = []
+  if (productss.data != undefined) {
+    val = productss.data
+  }
+  let s_id = localStorage.getItem('seller_id')
+  s_id = s_id.replace(/['"]+/g, '')
+
+  var products = []
+  val.map((v) => {
+    if (v.seller_id != s_id) {
+      products.push({
+        id: v._id,
+        name: v.name,
+        seller_id: v.seller_id,
+        description: v.description,
+        category: v.catagory,
+        subCategory: v.subCategory,
+        image: v.image,
+        image2: v.image2,
+        image3: v.image3,
+        unitName: v.unitName,
+        unitPrize: v.unitPrize,
+        bookingPercentage: v.bookingPercentage,
+        available: v.available,
+        minOrder: v.minOrder,
+        rating: v.rating,
+        availableDate: v.availableDate,
+        division: v.division,
+        district: v.district,
+        upazilla: v.upazilla,
+        isVerified: v.isVerified,
+        isAvailableNow: v.isAvailableNow,
+        rating: v.rating,
+        comments: v.comments,
+        question: v.question,
+        timestamp: v.timestamp,
+      })
     }
-
-    useEffect(async () => {
-        ShowProduct()
-    }, [])
-
-    let val = []
-    if (productss.data != undefined) {
-        val = productss.data
-    }
-
-    var products = []
-    val.map((v) =>
-        products.push({
-            id: v._id,
-            name: v.name,
-            seller_id: v.seller_i,
-            description: v.description,
-            category: v.catagory,
-            subCategory: v.subCategory,
-            image: v.image,
-            image2: v.image2,
-            image3: v.image3,
-            unitName: v.unitName,
-            unitPrize: v.unitPrize,
-            bookingPercentage: v.bookingPercentage,
-            available: v.available,
-            minOrder: v.minOrder,
-            rating: v.rating,
-            availableDate: v.availableDate,
-            division: v.division,
-            district: v.district,
-            upazilla: v.upazilla,
-            isVerified: v.isVerified,
-            isAvailableNow: v.isAvailableNow,
-            rating: v.rating,
-            comments: v.comments,
-            question: v.question,
-            timestamp: v.timestamp,
-        })
-    )
-    return products
+  })
+  return products
 }
 
 export default productData
--- src/views/Login.js
+++ src/views/Login.js
@@ -28,6 +28,10 @@ const Login = () => {
           'user_role',
           JSON.stringify(response.data.result.user_role)
         )
+        localStorage.setItem(
+          'seller_id',
+          JSON.stringify(response.data.result._id)
+        )
         const role = localStorage.getItem('user_role')
         console.log(role)
         if (role == '"admin"') {
@@ -44,72 +48,68 @@ const Login = () => {
   }
   return (
     <div className='SignInAndUp'>
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',
-              }}
-            >
-              <Container fluid>
-                <Card>
-                  <Card.Header>
-                    <Card.Title as='h4'>প্রবেশ করুন</Card.Title>
-                    <p className='card-category'>
-                      <a href='' rel='' target=''>
-                        গুগল অ্যাকাউন্ট .
-                      </a>
-                      ব্যবহার করুন
-                    </p>
-                  </Card.Header>
-                </Card>
-                <Card id='myForm1'>
-                  <Card.Body>
-                    <Form>
-                      <Form.Group controlId='email'>
-                        <Form.Label>ইমেইল</Form.Label>
-                        <Form.Control
-                          type='email'
-                          name='email'
-                          placeholder='আপনার ইমেইল অ্যাড্রেসটি এখানে লিখুন'
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                        />
-                        <Form.Text className='text-muted'>
-                          আপনার ইমেইলটি কখনোই কারও সাথে শেয়ার করা হবে না।
-                        </Form.Text>
-                      </Form.Group>
-                      <Form.Group controlId='password'>
-                        <Form.Label>পাসওয়ার্ড</Form.Label>
-                        <Form.Control
-                          type='password'
-                          name='password'
-                          placeholder='আপনার পাসওয়ার্ডটি এখানে লিখুন'
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                        />
-                      </Form.Group>
-                      <div className='SignipButton'>
-                        <Button variant='success' onClick={handleSubmit}>
-                          সাইন ইন
-                        </Button>
-                      </div>
-                    </Form>
-                    <p>একাউন্ট নেই?</p>
-                    <p className='card-category'>
-                      <a
-                        href='/register'
-                        rel='noopener noreferrer'
-                        target='_blank'
-                      >
-                        নতুন একাউন্ট তৈরি করুন
-                      </a>
-                    </p>
-                  </Card.Body>
-                </Card>
-              </Container>
-            </div>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <Container fluid>
+          <Card>
+            <Card.Header>
+              <Card.Title as='h4'>প্রবেশ করুন</Card.Title>
+              <p className='card-category'>
+                <a href='' rel='' target=''>
+                  গুগল অ্যাকাউন্ট .
+                </a>
+                ব্যবহার করুন
+              </p>
+            </Card.Header>
+          </Card>
+          <Card id='myForm1'>
+            <Card.Body>
+              <Form>
+                <Form.Group controlId='email'>
+                  <Form.Label>ইমেইল</Form.Label>
+                  <Form.Control
+                    type='email'
+                    name='email'
+                    placeholder='আপনার ইমেইল অ্যাড্রেসটি এখানে লিখুন'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <Form.Text className='text-muted'>
+                    আপনার ইমেইলটি কখনোই কারও সাথে শেয়ার করা হবে না।
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group controlId='password'>
+                  <Form.Label>পাসওয়ার্ড</Form.Label>
+                  <Form.Control
+                    type='password'
+                    name='password'
+                    placeholder='আপনার পাসওয়ার্ডটি এখানে লিখুন'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Form.Group>
+                <div className='SignipButton'>
+                  <Button variant='success' onClick={handleSubmit}>
+                    সাইন ইন
+                  </Button>
+                </div>
+              </Form>
+              <p>একাউন্ট নেই?</p>
+              <p className='card-category'>
+                <a href='/register' rel='noopener noreferrer' target='_blank'>
+                  নতুন একাউন্ট তৈরি করুন
+                </a>
+              </p>
+            </Card.Body>
+          </Card>
+        </Container>
+      </div>
     </div>
   )
 }
