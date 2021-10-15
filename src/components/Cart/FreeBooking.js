fcf944f1a5ba300141295106a95c6cd6121a1258
mohtasimhadi <mohtasimhadi@gmail.com>
2021-10-15 07:06:32 +0600

    Fee Booking Initial

--- src/components/Cart/FreeBooking.js
+++ src/components/Cart/FreeBooking.js
@@ -1,7 +1,7 @@
 import Cart from "./Cart"
 
 const FreeBooking = () => {
-    const cart = Cart('')
+    const cart = Cart()
     let freeBooking = []
     for(let i=0; i<8; i++){
         freeBooking.push(cart[i])
--- src/views/KrishiBazar.js
+++ src/views/KrishiBazar.js
@@ -6,6 +6,7 @@ import Banner from 'components/Banner'
 import CategoryCard from 'components/CategoryCard'
 import SearchBox from 'components/SearchBox'
 import NewArrival from 'components/Cart/NewArrival'
+import FreeBooking from 'components/Cart/FreeBooking'
 
 
 const KrishiBazar = () => {
@@ -24,12 +25,22 @@ const KrishiBazar = () => {
             <SearchBox />
         </Col>
         <Col md='9'>
+          <Row>
           <Card className="SignInAndUp" style={{ backgroundColor: '#F4FFE0' }}>
             <Card className="SignInAndUp" style={{ backgroundColor: '#F4FFE0', margin: '2px' }}><h3>নতুন পণ্য</h3></Card>
             <Row className="SignInAndUp">
               <NewArrival />
             </Row>
           </Card>
+          </Row>
+          <Row>
+          <Card className="SignInAndUp" style={{ backgroundColor: '#F4FFE0' }}>
+            <Card className="SignInAndUp" style={{ backgroundColor: '#F4FFE0', margin: '2px' }}><h3>ফ্রি বুকিং</h3></Card>
+            <Row className="SignInAndUp">
+              <FreeBooking />
+            </Row>
+          </Card>
+          </Row>
         </Col>
       </Row>
     </>
