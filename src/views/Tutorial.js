7cc762a0cf2525edb1e70bd4231a9bdc87cb985b
mohtasimhadi <mohtasimhadi@gmail.com>
2021-10-17 03:08:14 +0600

    Tutorial

--- src/views/Tutorial.js
+++ src/views/Tutorial.js
@@ -1,84 +1,22 @@
 import { Container, Row, Col, Card } from 'react-bootstrap'
-import রাফীর_ছবি from 'assets/img/about/rafi.jpg'
-import রাতুলের_ছবি from 'assets/img/about/ratul.jpg'
-import সাব্বিরের_ছবি from 'assets/img/about/sabbir.jpg'
+import Iframe from 'react-iframe'
 
 const about = () => {
-  return (
-    <Container fluid>
-      <Container
-        fluid
-        className='SignInAndUp'
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: 'auto',
-        }}
-      >
-        <Card style={{ width: '30%', margin: '5%' }}>
-          <Card.Body>
-            <Col>
-              <img
-                src={রাতুলের_ছবি}
-                alt='রাতুলের ছবি পাওয়া যায়নি'
+    return (
+        <Container fluid>
+            <Container
+                fluid
+                className='SignInAndUp'
                 style={{
-                  background: 'white',
-                  width: '320px',
-                  height: '420px',
-                  overflow: 'hidden',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: 'auto',
                 }}
-              />
-            </Col>
-            <Col style={{ marginLeft: '2%' }}>
-              <Row as='h3'>রাতুল মাহজাবিন</Row>
-              <Row>আইডি: ১৮০০৪২১২৩</Row>
-            </Col>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '30%' }}>
-          <Card.Body>
-            <Col>
-              <img
-                src={রাফীর_ছবি}
-                alt='রাফীর ছবি পাওয়া যায়নি'
-                style={{
-                  background: 'white',
-                  width: '320px',
-                  height: '420px',
-                  overflow: 'hidden',
-                }}
-              />
-            </Col>
-            <Col style={{ marginLeft: '2%' }}>
-              <Row as='h3'>মোহতাসিম হাদী রাফী</Row>
-              <Row>আইডি: ১৮০০৪২১৪৫</Row>
-            </Col>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: '30%', margin: '5%' }}>
-          <Card.Body>
-            <Col>
-              <img
-                src={সাব্বিরের_ছবি}
-                alt='সাব্বিরের ছবি পাওয়া যায়নি'
-                style={{
-                  background: 'white',
-                  width: '320px',
-                  height: '420px',
-                  overflow: 'hidden',
-                }}
-              />
-            </Col>
-            <Col style={{ marginLeft: '2%' }}>
-              <Row as='h3'>মো. সাব্বির রহমান</Row>
-              <Row>আইডি: ১৮০০৪২১৪২</Row>
-            </Col>
-          </Card.Body>
-        </Card>
-      </Container>
-    </Container>
-  )
+            >
+                <Iframe width="885" height="498" src="https://www.youtube.com/embed/z0RGTdVfFyw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></Iframe>            </Container>
+        </Container >
+    )
 }
 
 export default about
