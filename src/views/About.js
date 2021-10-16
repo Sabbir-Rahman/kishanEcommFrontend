4c0e030432125d50ad39c625398af8096942f0c6
mohtasimhadi <mohtasimhadi@gmail.com>
2021-10-17 01:06:28 +0600

    About page last

--- src/views/About.js
+++ src/views/About.js
@@ -1,22 +1,71 @@
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Card } from "react-bootstrap"
+import রাফীর_ছবি from 'assets/img/about/rafi.jpg'
 
 const about = () => {
     return (
-        <Container fluid className="SignInAndUp">
-            <Row>
-                <Col>
-                    Rafi
-                    ID: 180042145
-                </Col>
-                <Col>
-                    Ratul
-                    ID: 180042123
-                </Col>
-                <Col>
-                    Sabbir
-                    ID: 180042142
-                </Col>
-            </Row>
+        <Container fluid>
+            <Container className="SignInAndUp">
+                <Card className="SignInAndUp">
+                    <Card.Body>
+                        <Col >
+                            <img
+                                src={'রাতুলের_ছবি'}
+                                alt='রাতুলের ছবি পাওয়া যায়নি'
+                                style={{
+                                    background: 'white',
+                                    width: '100%',
+                                    height: 'auto',
+                                    overflow: 'hidden',
+                                }}
+                            />
+                        </Col>
+                        <Col style={{ marginLeft: '2%' }}>
+                            <Row as='h3'>রাতুল মাহজাবিন</Row>
+                            <Row>আইডি: ১৮০০৪২১২৩</Row>
+                        </Col>
+                    </Card.Body>
+                </Card>
+                <Card className="SignInAndUp">
+                    <Card.Body>
+                        <Col >
+                            <img
+                                src={রাফীর_ছবি}
+                                alt='রাফীর ছবি পাওয়া যায়নি'
+                                style={{
+                                    background: 'white',
+                                    width: '100%',
+                                    height: 'auto',
+                                    overflow: 'hidden',
+                                }}
+                            />
+                        </Col>
+                        <Col style={{ marginLeft: '2%' }}>
+                            <Row as='h3'>মোহতাসিম হাদী রাফী</Row>
+                            <Row>আইডি: ১৮০০৪২১৪৫</Row>
+                        </Col>
+                    </Card.Body>
+                </Card>
+                <Card className="SignInAndUp">
+                    <Card.Body>
+                        <Col >
+                            <img
+                                src={'সাব্বিরের ছবি'}
+                                alt='সাব্বিরের ছবি পাওয়া যায়নি'
+                                style={{
+                                    background: 'white',
+                                    width: '100%',
+                                    height: 'auto',
+                                    overflow: 'hidden',
+                                }}
+                            />
+                        </Col>
+                        <Col style={{ marginLeft: '2%' }}>
+                            <Row as='h3'>মো. সাব্বির রহমান</Row>
+                            <Row>আইডি: ১৮০০৪২১৪২</Row>
+                        </Col>
+                    </Card.Body>
+                </Card>
+            </Container>
         </Container>
     )
 }
