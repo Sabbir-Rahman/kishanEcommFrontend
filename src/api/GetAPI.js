a8db6e29004ad6897fb10e85058da850baa927b3
mohtasimhadi <mohtasimhadi@gmail.com>
2021-10-16 21:26:32 +0600

    All products added on front page

---
+++ src/api/GetAPI.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+const get = (data, message, link) => {
+    
+  }
+
+  export default get
--- src/views/KrishiBazar.js
+++ src/views/KrishiBazar.js
@@ -5,13 +5,8 @@ import Cart from 'components/Cart/Cart'
 import Banner from 'components/Banner'
 import CategoryCard from 'components/CategoryCard'
 import SearchBox from 'components/SearchBox'
-import NewArrival from 'components/Cart/NewArrival'
-import FreeBooking from 'components/Cart/FreeBooking'
-
 
 const KrishiBazar = () => {
-
-  const cart = Cart()
   return (
     <>
       {Banner(<CategoryCard />, bg)}
@@ -24,29 +19,14 @@ const KrishiBazar = () => {
         <Col md='2.5' style={{ margin: '5px' }}>
           <SearchBox />
         </Col>
-        <Col md='9'>
-          <Carousel>
-            <Carousel.Item>
-              <Row>
-                <Card className="SignInAndUp" style={{ backgroundColor: '#F4FFEE' }}>
-                  <Card className="SignInAndUp" style={{ margin: '2px', backgroundColor: '#F4FFEE' }}><h3>নতুন পণ্য</h3></Card>
-                  <Row className="SignInAndUp">
-                    <NewArrival />
-                  </Row>
-                </Card>
+        <Col md='9' className="SignInAndUp" style={{ backgroundColor: '#F4FFEE' }}>
+          <Row>
+            <Card className="SignInAndUp" style={{ backgroundColor: '#F4FFEE' }}>
+              <Row className="SignInAndUp">
+                {Cart()}
               </Row>
-            </Carousel.Item>
-            <Carousel.Item>
-              <Row>
-                <Card className="SignInAndUp" style={{ backgroundColor: '#F4FFEE' }}>
-                  <Card className="SignInAndUp" style={{ margin: '2px' }}><h3>ফ্রি বুকিং</h3></Card>
-                  <Row className="SignInAndUp">
-                    <FreeBooking />
-                  </Row>
-                </Card>
-              </Row>
-            </Carousel.Item>
-          </Carousel>
+            </Card>
+          </Row>
         </Col>
       </Row>
     </>
